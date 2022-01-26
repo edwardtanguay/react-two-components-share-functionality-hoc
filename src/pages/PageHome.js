e7f7105b2f823f26c078f5e5d6c0c5aa753df1b7
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-01-27 00:47:18 +0100

    fix production

--- src/pages/PageHome.js
+++ src/pages/PageHome.js
@@ -5,7 +5,7 @@ const PageHome = ({ InfoBox, employees, customers, config }) => {
 			<p>We currently have {employees.length} employees and {customers.length} customers.</p>
 			<InfoBox message="125 new registrations since January 1st." allowClosing="true" />
 			<InfoBox message="The sales department will be closed on Monday." allowClosing="true" />
-			{!config.getCurrentEnvironmentIdCode() !== 'production' && (
+			{config.getCurrentEnvironmentIdCode() !== 'production' && (
 				<div className="onlineVersion">
 					<p>see the <a href="https://react-two-components-share-functionality-hoc.netlify.app">online/production version of this app</a></p>
 				</div>
